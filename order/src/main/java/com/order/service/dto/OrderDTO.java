d507029a7c1cb48f98e08c8415e6fe8e9a7d5d63
RUSIRUDEVINDA <rusirud49@gmail.com>
2026-07-13 12:35:25 +0530

    feat: Implement order CRUD operations with DTO mapping

---
+++ order/src/main/java/com/order/service/dto/OrderDTO.java
@@ -0,0 +1,16 @@
+package com.order.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class OrderDTO {
+    private int id;
+    private int itemId;
+    private String  orderDate;
+    private int amount;
+}
---
+++ order/src/main/java/com/order/service/repo/OrderRepo.java
@@ -0,0 +1,10 @@
+package com.order.service.repo;
+
+import com.order.service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepo extends JpaRepository<Order, Integer> {
+    Order getOrderById(Integer orderId);
+}
