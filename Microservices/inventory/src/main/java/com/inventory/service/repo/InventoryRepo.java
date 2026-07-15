1f813e8e71d262c7d819c68e2f103bde2b6b64cb
RUSIRUDEVINDA <rusirud49@gmail.com>
2026-07-15 20:36:27 +0530

    feat(inventory): expose itemID query for order service integration

--- Microservices/inventory/src/main/java/com/inventory/service/repo/InventoryRepo.java
+++ Microservices/inventory/src/main/java/com/inventory/service/repo/InventoryRepo.java
@@ -2,8 +2,14 @@ package com.inventory.service.repo;
 
 import com.inventory.service.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.management.ValueExp;
 
 @Repository
 public interface InventoryRepo extends JpaRepository<Inventory, Integer> {
+    @Query(value = "SELECT * FROM inventory WHERE itemID = ?1", nativeQuery = true)
+    Inventory getInventoryByItemID(int itemID);
+
 }
