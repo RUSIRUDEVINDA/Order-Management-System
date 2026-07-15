f2549bc693306329795b311efcfe15951eac1ca7
RUSIRUDEVINDA <rusirud49@gmail.com>
2026-07-15 20:43:42 +0530

    feat(product): expose productID query for product service integration

--- Microservices/product/src/main/java/com/product/service/repo/ProductRepo.java
+++ Microservices/product/src/main/java/com/product/service/repo/ProductRepo.java
@@ -2,9 +2,11 @@ package com.product.service.repo;
 
 import com.product.service.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
-
+    @Query(value = "SELECT * FROM product WHERE productID = ?1", nativeQuery = true)
+    Product getProductByProductID(int productID);
 }
